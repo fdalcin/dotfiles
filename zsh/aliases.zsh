5412bbcf2b2a3ca977a247ea1bc8fbe97ee28e20
Felipe Dalcin <felipe@pressbooks.com>
2023-02-25 14:51:28 -0500

    Add phpstorm alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias copyssh="cat < $HOME/.ssh/id_ed25519.pub | pbcopy"
 alias reloadshell="source $HOME/.zshrc"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias phpstorm="open -a /Applications/PhpStorm.app"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
